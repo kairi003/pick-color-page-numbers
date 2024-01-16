e09bb6958e7924e36737a84fa4f7b18242d11370
kairi003 <kairi.satellite@gmail.com>
2024-01-17 00:16:49 +0900

    update

---
+++ script.js
@@ -0,0 +1,88 @@
+
+const MESSAGE_TYPE = {
+  LOAD: 'load',
+  PROGRESS: 'progress',
+  DONE: 'done',
+  ERROR: 'error'
+};
+
+const fileInput = document.getElementById('fileInput');
+const requireHyphenate = document.getElementById('requireHyphenate');
+const progress = document.getElementById('progress');
+const progressText = document.getElementById('progressText');
+const colorOutput = document.getElementById('colorOutput');
+const monoOutput = document.getElementById('monoOutput');
+const errorOutput = document.getElementById('errorOutput');
+
+const hyphenateRange = (arrSrc) => {
+  const arr = arrSrc.map(x => parseInt(x)).filter(x => !isNaN(x)).sort((a, b) => a - b);
+  const ranges = [];
+  let start = arr[0];
+  let end = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] === end + 1) {
+      end = arr[i];
+    } else {
+      ranges.push(start === end ? start : `${start}-${end}`);
+      start = end = arr[i];
+    }
+  }
+  ranges.push(start === end ? start : `${start}-${end}`);
+  return ranges.join(',');
+};
+const updateOutputShow = () => {
+  const format = requireHyphenate.checked ? hyphenateRange : arr => arr.join(',');
+  colorOutput.textContent = format(colorOutput.dataset.pages.split(','));
+  monoOutput.textContent = format(monoOutput.dataset.pages.split(','));
+}
+requireHyphenate.addEventListener('change', updateOutputShow);
+
+
+let worker = null;
+let taskId = 0;
+
+const messageHandle = (event) => {
+  console.log('Receive:', event);
+  const { id, type, data } = event.data;
+  if (id !== taskId) return;
+  switch (type) {
+    case MESSAGE_TYPE.LOAD:
+      progress.max = +data;
+      progress.value = 0;
+      progressText.textContent = `${progress.value} / ${progress.max}`;
+      break;
+    case MESSAGE_TYPE.PROGRESS:
+      progress.value++;
+      progressText.textContent = `${progress.value} / ${progress.max}`;
+      break;
+    case MESSAGE_TYPE.DONE:
+      const isMonoArray = data;
+      colorOutput.dataset.pages = isMonoArray
+        .map((isMono, index) => isMono ? '' : index + 1)
+        .filter(Boolean).join(',');
+      monoOutput.dataset.pages = isMonoArray
+        .map((isMono, index) => isMono ? index + 1 : '')
+        .filter(Boolean).join(',');
+      updateOutputShow();
+      break;
+    case MESSAGE_TYPE.ERROR:
+      errorOutput.textContent = data;
+      break;
+  }
+};
+
+fileInput.addEventListener('change', async event => {
+  console.log(event);
+  const file = fileInput.files[0];
+  if (!file) return;
+  const arrayBuffer = await file.arrayBuffer();
+  worker?.terminate();
+  worker = new Worker('./script.worker.mjs', {
+    type: 'module'
+  });
+  worker.addEventListener('message', messageHandle);
+  worker.postMessage({
+    id: ++taskId,
+    data: arrayBuffer
+  });
+});
---
+++ script.worker.mjs
@@ -0,0 +1,62 @@
+// await import('/pdfjs-dist/build/pdf.min.mjs');
+import * as pdfjsLib from './pdfjs-dist/build/pdf.min.mjs';
+
+const MESSAGE_TYPE = {
+  LOAD: 'load',
+  PROGRESS: 'progress',
+  DONE: 'done',
+  ERROR: 'error'
+};
+
+// Init worker
+self.window = self;
+self.document = {
+  fonts: self.fonts,
+  createElement: name => {
+    if (name === 'canvas') {
+      return new OffscreenCanvas(0, 0);
+    }
+  }
+};
+const worker = new Worker('./pdfjs-dist/build/pdf.worker.min.mjs', {type: 'module'});
+pdfjsLib.GlobalWorkerOptions.workerPort = worker;
+
+const isMonochrome = async (page) => {
+  console.log('Loaded Page:', page.pageNumber)
+  const viewport = page.getViewport({scale: 2});
+  const canvas = new OffscreenCanvas(viewport.width, viewport.height);
+  const ctx = canvas.getContext('2d');
+  const renderTask = page.render({
+    canvasContext: ctx,
+    viewport
+  });
+  await renderTask.promise;
+  console.log('Rendered Page:', page.pageNumber)
+  const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  const {data} = imageData;
+  return [...Array(data.length / 4).keys()]
+    .every(i => data[4 * i] === data[4 * i + 1] && data[4 * i] === data[4 * i + 2]);
+}
+
+self.onmessage = async (event) => {
+  try {
+    console.log('worker received message', event);
+    const {id, data} = event.data;
+    const pdf = await pdfjsLib.getDocument({
+      data,
+      cMapUrl: "./pdfjs-dist/cmaps/",
+      cMapPacked: true,
+      ownerDocument: document
+    }).promise;
+    console.log('Loaded PDF')
+    self.postMessage({id, type: MESSAGE_TYPE.LOAD, data: pdf.numPages});
+    const pageNumbers = [...Array(pdf.numPages).keys()].map(i => i + 1);
+    const postProgress = s => (self.postMessage({id, type: MESSAGE_TYPE.PROGRESS}), s);
+    const isMonochromeArray = await Promise.all(
+      pageNumbers.map(p=>pdf.getPage(p).then(isMonochrome).then(postProgress)));
+    self.postMessage({id, type: MESSAGE_TYPE.DONE, data: isMonochromeArray});
+  } catch (e) {
+    console.error(e);
+    self.postMessage({id, type: MESSAGE_TYPE.ERROR, data: e});
+  }
+};
